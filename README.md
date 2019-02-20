9ee6e9337d2497e098070d25e4d7238f1be38c8d
Mike Langendorf <mike.langendorf@gmail.com>
2019-02-20 14:29:24 +0100

    second commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#Ionic 4 iOS Slow Rendering Issue 
+
+This demo shows a rendering issue of an ionic 4 project with nested ion-grid into a div.
+
+##How to install
+
+```
+$> npm install
+$> ionic cordova platform add ios
+$> ionic cordova prepare ios
+```
+
+Run in the iOS Simulator
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+
+  posts: Post[];
+
+  constructor(private http: HttpClient) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.posts = await this.http.get<Post[]>(`https://jsonplaceholder.typicode.com/posts`).toPromise();
+  }
+
+  trackByPostId(index: number, post: Post) {
+    return post.id;
+  }
+
+}
